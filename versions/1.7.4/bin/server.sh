308af6d2287eee05a111a62d31898aba190d0cf7
Dmitry Zubelevich <dz@jelastic.com>
2016-06-21 11:48:18 +0300

    JE-26997 [Enable MaxPermSize in container if RAM is > 800MB ( for Java 6, 7)] - adjustment for cartridges

--- versions/1.7.4/bin/server.sh
+++ versions/1.7.4/bin/server.sh
@@ -78,5 +78,8 @@ WWW_PATH=$ORIENTDB_HOME/www
 ORIENTDB_SETTINGS="-Dprofiler.enabled=true"
 JAVA_OPTS_SCRIPT="-Djna.nosys=true -XX:+HeapDumpOnOutOfMemoryError -Djava.net.preferIPv4Stack=true -Djava.awt.headless=true -Dfile.encoding=UTF8 -Drhino.opt.level=9"
 
-$JAVA $JAVA_OPTS $JAVA_OPTS_SCRIPT $ORIENTDB_SETTINGS -Djava.net.preferIPv4Stack=true -Djava.util.logging.config.file="$LOG_FILE" -Dorientdb.config.file="$CONFIG_FILE" -Dorientdb.www.path="$WWW_PATH" -Dorientdb.build.number="UNKNOWN@r${buildNumber}; 2014-06-23 19:29:10+0200" -cp "$ORIENTDB_HOME/lib/orientdb-server-1.7.4.jar:$ORIENTDB_HOME/lib/*" com.orientechnologies.orient.server.OServerMain
+memory_total=`free -m | grep Mem | awk '{print $2}'`;
+let MAXDISKCACHE=memory_total-67;
+
+$JAVA $JAVA_OPTS $JAVA_OPTS_SCRIPT $ORIENTDB_SETTINGS -Djava.net.preferIPv4Stack=true -Djava.util.logging.config.file="$LOG_FILE" -Dorientdb.config.file="$CONFIG_FILE" -Dorientdb.www.path="$WWW_PATH" -Dorientdb.build.number="UNKNOWN@r${buildNumber}; 2014-06-23 19:29:10+0200" -cp "$ORIENTDB_HOME/lib/orientdb-server-1.7.4.jar:$ORIENTDB_HOME/lib/*" com.orientechnologies.orient.server.OServerMain -Dstorage.diskCache.bufferSize=$MAXDISKCACHE
 
